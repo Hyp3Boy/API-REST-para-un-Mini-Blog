56b8319bb93c7acfe6644d2a778effd222a1510a
Lenin Chavez <leninthexd@gmail.com>
2025-10-18 14:22:57 -0500

    fix: config change to accept a complete env

--- src/api_rest_mini_blog/config.py
+++ src/api_rest_mini_blog/config.py
@@ -5,6 +5,10 @@ class Settings(BaseSettings):
     ENVIRONMENT: str = Field(default="development", description="Environment type")
     DATABASE_URL: str = Field(default="", description="Database URL")
     TEST_DATABASE_URL: str = Field(default="", description="Test Database URL")
+    postgres_user: str = Field(default="", description="Postgres user")
+    postgres_password: str = Field(default="", description="Postgres password")
+    postgres_db: str = Field(default="", description="Postgres database")
+    postgres_host: str = Field(default="", description="Postgres host")
 
     model_config = SettingsConfigDict(env_file='.env')
 
